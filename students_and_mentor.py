bad920812e5031ea95776c75c2abebfc49aa4206
Garik <igor@koleso40.ru>
2025-08-12 20:40:50 +0300

    The first task is solved.

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -33,17 +33,6 @@ class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-
-print(best_student.grades)
 
 lecturer = Lecturer('Иван', 'Иванов')
 reviewer = Reviewer('Пётр', 'Петров')
