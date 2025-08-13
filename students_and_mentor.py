93d9a1deb3e847265d71e3dece51e95db7234594
Garik <igor@koleso40.ru>
2025-08-13 18:06:22 +0300

    The second task is solved.

--- students_and_mentor.py
+++ students_and_mentor.py
@@ -7,6 +7,12 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecture(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            lecturer.grades.setdefault(course, grade)
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -27,16 +33,30 @@ class Mentor:
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
+        self.grades = {}
 
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
 
-
+# Проверка к задаче №1
 lecturer = Lecturer('Иван', 'Иванов')
 reviewer = Reviewer('Пётр', 'Петров')
 print(isinstance(lecturer, Mentor)) # True
 print(isinstance(reviewer, Mentor)) # True
 print(lecturer.courses_attached)    # []
 print(reviewer.courses_attached)    # []
+
+# Проверка к задаче №2
+student = Student('Алёхина', 'Ольга', 'Ж')
+student.courses_in_progress += ['Python', 'Java']
+lecturer.courses_attached += ['Python', 'C++']
+reviewer.courses_attached += ['Python', 'C++']
+
+print(student.rate_lecture(lecturer, 'Python', 7))   # None
+print(student.rate_lecture(lecturer, 'Java', 8))     # Ошибка
+print(student.rate_lecture(lecturer, 'С++', 8))      # Ошибка
+print(student.rate_lecture(reviewer, 'Python', 6))   # Ошибка
+
+print(lecturer.grades)  # {'Python': [7]}  
